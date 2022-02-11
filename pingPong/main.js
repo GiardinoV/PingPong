1f06607cd26128dcdca079ff96b5accaa42d7557
virginia <vgiardino@outlook.es>
2022-02-11 10:53:02 -0300

    Agrego funcion colision y queda ping pong funcionando

--- pingPong/main.js
+++ pingPong/main.js
@@ -29,11 +29,33 @@
         this.direction = 1;
         board.ball = this;
         this.kind = "circle";
+        this.bounce_angle = 0;
+        this.max_bounce_angle = Math.PI / 12;
+        this.speed = 3;
 }
 self.Ball.prototype = {
     move: function(){
         this.x += (this.speed_x * this.direction);
         this.y += (this.speed_y * this.direction)
+    },
+    get width(){
+        return this.radius * 2;
+    },
+    get height(){
+        return this.radius * 2;
+    },
+    collision: function(bar){
+        var relative_intersect_y = (bar.y + (bar.height / 2)) - this.y;
+
+        var normalized_intersect_y = relative_intersect_y / (bar.height / 2);
+
+        this.bounce_angle = normalized_intersect_y * this.max_bounce_angle;
+        
+        this.speed_y = this.speed * -Math.sin(this.bounce_angle);
+        this.speed_x = this.speed * Math.cos(this.bounce_angle);
+
+        if(this.x > (this.board.width /2)) this.direction = -1;
+        else this.direction = 1;
     }
 }
 })();
@@ -80,18 +102,56 @@ self.Ball.prototype = {
                 draw(this.ctx, el);                
             }
         },
+        check_collisions: function(){
+            for (let i = this.board.bars.length - 1; i >= 0; i--) {
+                var bar = this.board.bars[i];
+                if (hit(bar, this.board.ball)) {
+                    this.board.ball.collision(bar);
+                }
+                
+            }
+        },
         play: function(){
             if(this.board.playing){
             this.clean();
             this.draw();
             this.board.ball.move();
+            this.check_collisions();
             }  
 
         }
     }
 
+    function hit(a, b){
+        //Verifica si a colisiona con b
+        var hit = false;
+
+        //Colisiones horizontales
+        if(b.x + b.width >= a.x && b.x < a.x + a.width){
+            //Colisiones verticales
+            if(b.y + b.height >= a.y && b.y < a.y + a.height){
+                hit = true;
+            }
+        }
+            //Colsion de a con b
+        if(b.x <= a.x && b.x + b.width >= a.x + a.width){
+            if(b.y <= a.y && b.y + b.height >= a.y + a.height){
+                hit = true;
+            }
+        }
+
+        //Colision b con a
+        if(a.x <= b.x && a.x + a.width >= b.x + b.width){
+            if(a.y <= b.y && a.y + a.height >= b.y + b.height){
+                hit = true;
+            }
+        }
+
+        return hit;
+    }
+
     function draw(ctx, element){
-        // if (element !== null && element.hasOwnProperty("kind")) generaba error
+        // if (element !== null && element.hasOwnProperty("kind")) **generaba error
         switch(element.kind){
             case "rectangle":
                 ctx.fillRect(element.x, element.y, element.width, element.height);
